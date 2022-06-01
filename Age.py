8fb051f75411952c289c7f02bb93fe52cc3e5fd6
parkc25 <parkc25@greenhill.org>
2022-06-01 09:55:55 -0500

    ok

---
+++ Age.py
@@ -0,0 +1,12 @@
+#Christan Park
+#Calculate Age
+#get user year and current year 
+#import things
+import os
+os.system("cls")
+by=int(input('year you were born, '))
+currentYear=2022
+age=currentYear-by
+print('Your age is ',age)
+if age > 50:
+    print("You are old")
