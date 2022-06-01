172a78b523aa8a005f10887d0d993fe8e3b25377
parkc25 <parkc25@greenhill.org>
2022-06-01 09:04:02 -0500

    ok

--- test.py
+++ test.py
@@ -3,3 +3,11 @@ os.system("cls")
 print("hello")
 print("how are you doing")
 print("i'm doing good")
+
+meal = "eggs and ham"
+print("Breakfast: "+ meal)
+
+integer = 5
+float = 3.25
+result = integer + float 
+print(result)
