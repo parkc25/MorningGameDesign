2b714deb8196d8e94b623316a204bd404a932ca4
parkc25 <parkc25@greenhill.org>
2022-06-01 10:08:54 -0500

    ok

--- BMI.py
+++ BMI.py
@@ -3,15 +3,14 @@
 import os
 os.system("cls")
 #put height in inches 
-height=int(input('height in inches'))
-weight=int(input('weight in pounds'))
+height=int(input('height in inches '))
+weight=int(input('weight in pounds '))
 BMI=weight/(height*height)*703
-if BMI < 18.5
-    print("You are under")
-
-by=int(input('year you were born, '))
-currentYear=2022
-age=currentYear-by
-print('Your age is ',age)
-if age > 50:
-    print("You are old")
+if BMI<18.5:
+    print("You are underweight")
+if BMI<20:
+    print("You are normal")
+if BMI<30:
+    print("You are obese")
+else:
+    print("You are obese")
