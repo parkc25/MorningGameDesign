016e4f829239a67cdd4270e27a13c7b4103c687d
parkc25 <parkc25@greenhill.org>
2022-06-01 10:05:16 -0500

    ok

--- BMI.py
+++ BMI.py
@@ -1,1 +1,17 @@
 #Christan Park 
+#import things
+import os
+os.system("cls")
+#put height in inches 
+height=int(input('height in inches'))
+weight=int(input('weight in pounds'))
+BMI=weight/(height*height)*703
+if BMI < 18.5
+    print("You are under")
+
+by=int(input('year you were born, '))
+currentYear=2022
+age=currentYear-by
+print('Your age is ',age)
+if age > 50:
+    print("You are old")
