dcee05a86a9f29d6287ea8b2e467b3d011e6af64
parkc25 <parkc25@greenhill.org>
2022-06-02 09:39:00 -0500

    ok

--- StringsAssignment.py
+++ StringsAssignment.py
@@ -1,3 +1,10 @@
 #Christan Park
+import os
+os.system("cls")
+#get first middle and last letters
 a = "James"
-print(len(a))
+print(len(a)) #number of letter
+print(a[0], end='') 
+print(a[2], end='') 
+print(a[4])
+
