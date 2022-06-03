9e5ab9a2095d416875f99c4555fdbc30983e55fe
parkc25 <parkc25@greenhill.org>
2022-06-03 10:51:40 -0500

    ok

--- WordGuessAssignment.py
+++ WordGuessAssignment.py
@@ -7,29 +7,37 @@ import os
 import random
 os.system('cls')
 
-print("***********************************")
+#Game with Unlimited Guesses 
+
+print("****************************************")
 print("Welcome to Guess The Animal!")
 name = input("What is your name? ")
 print("Hi",name,", here are the rules:")
 print("1. Guess a animal")
-print("2. You can guess until you win!")
+print("2. You have unlimited guesses")
+print("3. Guess till you win!")
 print("Good Luck",name,"!")
-print("***********************************")
+print("****************************************")
  
 list = ['lion','tiger','turtle','penguin','shark','dolphin','whale','bear','wolf']
 
 random_word = random.choice(list)
+print("The random word is", random_word)
 guess = ''
 
 print("")
 while guess != random_word:
     guess = input("Guess an animal: ")
-    print("Sorry that is incorrect, try again!")
-
-print("You win! The coreect word was", random_word)
-print("***********************************")
-
-
+    if guess != random_word:
+        print("Sorry that is incorrect, try again!")
+    if guess == random_word:
+        print("You win! The coreect word was", random_word)
+print("")
+print("Do you want to play again?")
+play_again = input("Type Y if yes or N if no")
+if play_again = "N":
+    repeat = False
+print("****************************************")
 
 
 
