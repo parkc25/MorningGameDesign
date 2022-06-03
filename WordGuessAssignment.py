601b14689ea71146b711c13c81dae35bd9a6e7db
parkc25 <parkc25@greenhill.org>
2022-06-03 10:40:30 -0500

    ok

--- WordGuessAssignment.py
+++ WordGuessAssignment.py
@@ -12,14 +12,24 @@ print("Welcome to Guess The Animal!")
 name = input("What is your name? ")
 print("Hi",name,", here are the rules:")
 print("1. Guess a animal")
-print("2. You will have 10 guesses")
+print("2. You can guess until you win!")
 print("Good Luck",name,"!")
 print("***********************************")
  
 list = ['lion','tiger','turtle','penguin','shark','dolphin','whale','bear','wolf']
 
 random_word = random.choice(list)
-print('our random word', random_word)
+guess = ''
+
+print("")
+while guess != random_word:
+    guess = input("Guess an animal: ")
+    print("Sorry that is incorrect, try again!")
+
+print("You win! The coreect word was", random_word)
+print("***********************************")
+
+
 
 
 
