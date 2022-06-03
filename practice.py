26e2f738ea2ec5b8817c6bf5f5b3daf97290d68f
parkc25 <parkc25@greenhill.org>
2022-06-02 22:56:17 -0500

    ok

--- practice.py
+++ practice.py
@@ -6,15 +6,17 @@ word5 = input('Your word is ')
 lower = []
 upper = []
 for characters in word5:
-    if characters.islower():
-        lower.append(characters) #append moves characters to "free space" 
+    if characters.islower(): #islower -> returns True if all characters in the string are lower case
+        lower.append(characters) #append moves characters to end of list
     else:
-        upper.append(characters)
+        upper.append(characters) #append moves characters to end of list
 
 print(lower)
 print(upper)
 sortedWord = ''.join(lower + upper) # join() takes all items and joines them into one string
 print('Sorted word is ', sortedWord)
+
+#!!!!! Why need ''. before join???? -> https://stackoverflow.com/questions/14868763/global-name-join-is-not-defined-django
 
 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','U','R','S','T','U','V','W','X','Y','Z'
 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
