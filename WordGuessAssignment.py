803d0ac0456fa640653c5badecbf85e7746ba821
parkc25 <parkc25@greenhill.org>
2022-06-06 10:59:27 -0500

    ok

--- WordGuessAssignment.py
+++ WordGuessAssignment.py
@@ -28,70 +28,72 @@ while all_games:
     print("")
     print("***************************************************************************")
     print("")
-    while True: 
+    while all_games:
         choice = input("What game do you want to play? 1, 2, or 3? ")
-        print("")
-        try:
-            choice=int(choice)
-            if choice == 1:
-                number_game = True
-                while number_game:
-                    print("***************************************************************************") 
-                    print("") 
-                    print("Welcome to Guess The Number!")
-                    print("Hi",name,", here are the rules:")
-                    print("1. Guess a number from 0 to 50")
-                    print("2. You have 5 guesses")
-                    print("3. After each incorrect guess you will recieve a hint of lower or higher")
-                    print("Good Luck",name,"!")
-                    print("")
-                    print("***************************************************************************")
-                    print("")
+        choice=int(choice)
+        if choice == 1:
+            number_game = True
+            while number_game:
+                print("***************************************************************************") 
+                print("") 
+                print("Welcome to Guess The Number!")
+                print("Hi",name,", here are the rules:")
+                print("1. Guess a number from 0 to 50")
+                print("2. You have 5 guesses")
+                print("3. After each incorrect guess you will recieve a hint of lower or higher")
+                print("Good Luck",name,"!")
+                print("")
+                print("***************************************************************************")
+                print("")
 
-                    random_number = random.randint(1,50) #allows computer to choose a number between 1 and 50
-                    number_of_guesses = 0
-                    #print("The random number is", random_number)
+                random_number = random.randint(1,50) #allows computer to choose a number between 1 and 50
+                number_of_guesses = 0
+                #print("The random number is", random_number)
 
-                    while number_of_guesses < 5: #makes user limited to 5 guesses
-                        guess = int(input("Guess a number: ")) #make into int to allow it to be on the same line as a number
-                        number_of_guesses += 1
-                        if guess < random_number:
-                            print('The random number is higher')
-                        if guess > random_number:
-                            print('The random number is lower')
-                        if guess == random_number:
-                            break
-                    print("")
+                while number_of_guesses < 5: #makes user limited to 5 guesses
+                    guess = int(input("Guess a number: ")) #make into int to allow it to be on the same line as a number
+                    number_of_guesses += 1
+                    if guess < random_number:
+                        print('The random number is higher')
+                    if guess > random_number:
+                        print('The random number is lower')
                     if guess == random_number:
-                        print("You guessed the number! The number was " + str(random_number) + "!") #make number string to allow to be on the same line as letters
-                    else:
-                        print('Sorry, you did not guess the number. The number was ' + str(random_number) + ".")
+                        break
+                print("")
+                if guess == random_number:
+                    print("You guessed the number! The number was " + str(random_number) + "!") #make number string to allow to be on the same line as letters
+                else:
+                    print('Sorry, you did not guess the number. The number was ' + str(random_number) + ".")
+                print("")
+                print("***************************************************************************")
+                print("")
+                print("Thank you for playing! ")
+                print("Would you like to play the number game again, choose a new game. or not play at all? ")
+                print("To play the number game again type '1'")
+                print("To play another game type '2'")
+                print("To stop playing type '3'")
+                play_again = int(input("Type your response here: "))
+                if play_again == 1: 
+                    number_game=True
+                    print("")
+                elif play_again == 2: 
+                    all_games=True #SOMETHING WRONG HERE
+                    print("")
+                elif play_again == 3:
+                    print("Ok, see you later and thank you for playing!")
+                    all_games = False
+                    number_game = False
                     print("")
                     print("***************************************************************************")
+                else:
+                    print("Ok, see you later and thank you for playing!")
+                    all_games = False
+                    number_game = False
                     print("")
-                    print("Thank you for playing! ")
-                    print("Would you like to play the number game again, choose a new game. or not play at all? ")
-                    print("To play the number game again type 'number game'")
-                    print("To play another game type 'new game'")
-                    print("To stop playing type 'stop'")
-                    play_again = input("Type your response here: ")
-                    if play_again == "number game" or "Number game" or "Number Game": 
-                        number_game=True
-                        print("")
-                    elif play_again == "new game" or "New game" or "New Game": 
-                        all_game=True
-                        print("")
-                    else:
-                        print("Ok, see you later and thank you for playing!")
-                        number_game = False
-                        print("")
-                        print("***************************************************************************")
-            elif choice == 2:
-                Ksjd
-            elif choice == 3:
-                jjh
-            else:
-                print("Sorry we don't have that game please choose from 1, 2, or 3!")
-        except:
+                    print("***************************************************************************")
+        elif choice == 2:
+            Ksjd
+        elif choice == 3:
+            jjh
+        else:
             print("Sorry we don't have that game please choose from 1, 2, or 3!")
-
