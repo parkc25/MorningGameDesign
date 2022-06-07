04bf12c98b95be0124c3f5566230c124873f85b9
parkc25 <parkc25@greenhill.org>
2022-06-07 10:31:14 -0500

    ok

---
+++ files.py
@@ -0,0 +1,19 @@
+
+
+import os, datetime
+Date = datetime.datetime.now() 
+screLine = 120 
+print(Date) 
+print(Date.strftime('%m  %d  %Y'))
+print(screLine) 
+myFile = open("scre.txt", 'w') 
+myFile.write(screLine)
+myFile.close() 
+myFile=open("scre.txt", 'a') 
+myFile = open("scre.txt", 'r')  
+lines = myFile.readlines()
+print(lines)
+for line in myFile.readlines():
+	print(line)
+myFile.close()
+
