ee2ac097f0c40a4f61827299dcea0dff684c754a
parkc25 <parkc25@greenhill.org>
2022-06-08 15:26:58 -0500

    ok

--- BMI.py
+++ BMI.py
@@ -2,12 +2,12 @@
 #import things
 import os
 os.system("cls")
-#put height in inches 
-height=int(input('height in inches '))
-weight=int(input('weight in pounds '))
-BMI=weight/(height*height)*703
-print('Your BMI is',BMI)
-if BMI<=18.5:
+height=int(input('height in inches ')) #use int to make string of number into integer
+weight=int(input('weight in pounds ')) #use int to make string of number into integer
+BMI=weight/(height*height)*703 #equation for BMI
+print('Your BMI is',BMI) #tell them their BMI
+#use these comparisons to find if they are underwieght, normal, or obese
+if BMI<=18.5: #<= -> less than or equal to 
     print("You are underweight")
 elif BMI<=25:
     print("You are normal")
