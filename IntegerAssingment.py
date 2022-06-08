9584fb7c41cc8cbee377cae3219aff20890c16cd
parkc25 <parkc25@greenhill.org>
2022-06-08 15:32:41 -0500

    ok

--- IntegerAssingment.py
+++ IntegerAssingment.py
@@ -5,17 +5,17 @@ os.system("cls")
 
 #Question 1: Is the number even or odd?
 
-num = int(input("Enter any number it test whether it is off or even: "))
-if(num % 2 == 0):
+num = int(input("Enter any number it test whether it is off or even: ")) #ask user to put in number
+if(num % 2 == 0): #equation ask if when number is divided by 2 it equals zero. If so, it is even if not odd.
     print("The number is even")
 else: 
     print("The provided number is off")
 
 
-#Question 2: Is the number a multiple of 3 or 5?
+#Question 2: Is the number a multiple of 3 and 5?
 
-num = int(input("Enter any number it test whether it is a multiple of 3 or 5: "))
-if(num % 3 == 0 and num % 5 == 0):
+num = int(input("Enter any number it test whether it is a multiple of 3 and 5: ")) #ask user to put in number
+if(num % 3 == 0 and num % 5 == 0): #test if when number is divided by 3 and 5 it is equal to zero. If so, it can be divided. 
     print("The number is a multiple of 3 and 5")
-else: 
+else:
     print("The number is not a multiple of 3 and 5")
