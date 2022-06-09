2b801f43cb5e0e8065f9e75f1f43b0f56139202e
parkc25 <parkc25@greenhill.org>
2022-06-08 21:14:23 -0500

    ok

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -30,11 +30,8 @@ def level1():
     while number_game:
         print("***************************************************************************") 
         print("") 
-        print("Welcome to Guess a Number from 1-25!")
-        print("Hi",name,", here are the rules:")
-        print("1. Guess a number from 1 to 25")
-        print("2. You have 5 guesses")
-        print("3. After each incorrect guess you will recieve a hint of lower or higher")
+        print("") 
+        print("Hi",name,", welcome to level 1")
         print("Good Luck",name,"!")
         print("")
         print("***************************************************************************")
@@ -87,11 +84,7 @@ def level2():
     while number_game:
         print("***************************************************************************") 
         print("") 
-        print("Welcome to Guess a Number from 1-50!")
-        print("Hi",name,", here are the rules:")
-        print("1. Guess a number from 1 to 50")
-        print("2. You have 8 guesses")
-        print("3. After each incorrect guess you will recieve a hint of lower or higher")
+        print("Hi",name,", welcome to level 2")
         print("Good Luck",name,"!")
         print("")
         print("***************************************************************************")
@@ -144,11 +137,7 @@ def level3():
     while number_game:
         print("***************************************************************************") 
         print("") 
-        print("Welcome to Guess a Number from 1-100!")
-        print("Hi",name,", here are the rules:")
-        print("1. Guess a number from 1 to 100")
-        print("2. You have 10 guesses")
-        print("3. After each incorrect guess you will recieve a hint of lower or higher")
+        print("Hi",name,", welcome to level 3")
         print("Good Luck",name,"!")
         print("")
         print("***************************************************************************")
@@ -219,15 +208,59 @@ while start_game:
             choice = input("What game do you want to play? 1, 2, or 3? ")
             choice=int(choice)
             if choice == 1:
-                level1()
+                print("***************************************************************************") 
+                print("") 
+                print("") 
+                print("Hi",name,", welcome to level 1")
+                print("Good Luck",name,"!")
+                print("")
+                print("***************************************************************************")
+                print("")
+
+                random_number = random.randint(1,25) #allows computer to choose a number between 1 and 50
+                number_of_guesses = 0
+
+                while number_of_guesses < 5: #makes user limited to 5 guesses
+                    guess = int(input("Guess a number: ")) #make into int to allow it to be on the same line as a number
+                    number_of_guesses += 1
+                    if guess < random_number:
+                        print('The random number is higher')
+                    if guess > random_number:
+                        print('The random number is lower')
+                    if guess == random_number:
+                        break
+                print("")
+                if guess == random_number:
+                    print("You guessed the number! The number was " + str(random_number) + "!") #make number string to allow to be on the same line as letters
+                else:
+                    print('Sorry, you did not guess the number. The number was ' + str(random_number) + ".")
+
             if choice == 2:
-                level2()
+                print("***************************************************************************") 
+                print("") 
+                print("Hi",name,", welcome to level 2")
+                print("Good Luck",name,"!")
+                print("")
+                print("***************************************************************************")
+                print("")
+
+                random_number = random.randint(1,50) #allows computer to choose a number between 1 and 50
+                number_of_guesses = 0
             if choice == 3:
-                level3()
+                print("***************************************************************************") 
+                print("") 
+                print("Hi",name,", welcome to level 3")
+                print("Good Luck",name,"!")
+                print("")
+                print("***************************************************************************")
+                print("")
+
+                random_number = random.randint(1,100) #allows computer to choose a number between 1 and 50
+                number_of_guesses = 0
             else:
                 print("Sorry we don't have that game please choose from 1, 2, or 3!")
 
-            high=1880
+            high=1800
             score= 2000-40*number_of_guesses 
             print(name+ " your score is "+str(score))    
             input("press enter") #spaces out program a bit, makes them press enter to continue 
