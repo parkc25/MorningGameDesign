d07931f03cfa0e88f7bc3acb7975074458f824ae
parkc25 <parkc25@greenhill.org>
2022-06-13 11:10:32 -0500

    ok

--- circlemenu.py
+++ circlemenu.py
@@ -39,7 +39,6 @@ char = pygame.transform.scale(char, (50, 50))
 # pygame.display.update()
 # pygame.time.delay(5000)
 
-
 #square Var
 hb=50
 wb=50
@@ -68,7 +67,7 @@ Game = False
 
 def menu():
     #gets names for stuff on menu
-    Title = TITLE_FONT.render("Circle eats Square", 1, colors.get("blue"))
+    Title = TITLE_FONT.render("Circle Eats Square", 1, colors.get("blue"))
     screen.fill(colors.get("white"))
     xd = WIDTH//2 - (Title.get_width()//2)
     screen.blit(Title, (xd, 50))
@@ -107,9 +106,9 @@ def menu():
                 if Button_instruct.collidepoint((mx, my)):
                     Instructions()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                mousePos = pygame.mouse.get_pos()
-                sx = mousePos[0]
-                sy = mousePos[1]
+                mousePos2 = pygame.mouse.get_pos()
+                sx = mousePos2[0]
+                sy = mousePos2[1]
                 if Button_settings.collidepoint((sx, sy)):
                     settings()
             elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -125,52 +124,6 @@ def menu():
             # elif Button_exit.collidepoint((mx, my)):
             #     pass
 
-def settings():
-    Title = TITLE_FONT.render("Instructions", 1, colors.get("blue"))
-    text1 = MENU_FONT.render("Yes", 1, colors.get("blue"))
-    text2 = MENU_FONT.render("No", 1, colors.get("blue"))
-
-    #fills screen with white
-    screen.fill(colors.get("white"))
-
-    #creating button options
-    Button_1 = pygame.Rect(200, 400, 100, 50)
-    Button_2 = pygame.Rect(400, 400, 100, 50)
-    pygame.draw.rect(screen, colors.get("limeGreen"), Button_1)
-    pygame.draw.rect(screen, colors.get("limeGreen"), Button_2)
-
-    #Instructions
-    myFile = open("instruction.txt", "r")
-    content = myFile.readlines()
-
-    #var to controll change of line
-    yinstructions = 150
-    for line in content:
-        Instruc = MENU_FONT.render(line[0:-1], 1, colors.get("blue"))
-        screen.blit(Instruc, (40, yinstructions))
-        pygame.display.update()
-        pygame.time.delay(50)
-        yinstructions += 40
-
-    myFile.close()
-
-    # #renderig fonts to the screen
-    # xd = WIDTH//2 - (Title.get_width()//2)
-    # screen.blit(Title, (xd, 50))
-    # screen.blit(text1, (225, 410))
-    # screen.blit(text2, (425, 410))
-
-    # screen.fill(colors.get("white"))
-    # Title = TITLE_FONT.render("Settings", 1, colors.get("blue"))
-    # text_music = MENU_FONT.render("Music: ", 1, colors.get("blue"))
-    # text1 = MENU_FONT.render("Yes", 1, colors.get("blue"))
-    # text2 = MENU_FONT.render("No", 1, colors.get("blue"))
-
-    # #creating button options
-    # Button_1 = pygame.Rect(100, 150, 75, 75)
-    # Button_2 = pygame.Rect(100, 150, 75, 75)
-    # pygame.draw.rect(screen, colors.get("limeGreen"), Button_1)
-    # pygame.draw.rect(screen, colors.get("limeGreen"), Button_2)  
 
 def Instructions():
     #rendering text objects
@@ -222,61 +175,73 @@ def Instructions():
                     return True
                 if Button_2.collidepoint((mx, my)):
                     return False
-menu()
-run = Instructions()
 
-while run:
-    # screen.fill(backgrnd)
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            run=False
-            print("Y quit")
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mousePos = pygame.mouse.get_pos()
-            mx = mousePos[0]
-            my = mousePos[1]
-    screen.blit(bg, (0,0))
-    keys= pygame.key.get_pressed() #this is a list
-    #mve square
-    if keys[pygame.K_RIGHT] and square.x < WIDTH -(wb):
-        square.x += speed
-        charx += speed
-    if keys[pygame.K_LEFT] and  square.x > speed:
-        square.x -= speed
-        charx -= speed
-    if keys[pygame.K_UP] and square.y >speed:   #means clser t 0
-        square.y -= speed
-        chary -= speed
-    if keys[pygame.K_DOWN] and square.y <HEIGHT -hb:  #means clser t max value HEIGHT
-        square.y += speed
-        chary += speed
-        #mve Circle
-    if keys[pygame.K_d] and cx < WIDTH -(rad):
-        cx += speed
-        insSquare.x += speed
-    if keys[pygame.K_a] and  cx > (speed+rad):
-        cx -= speed
-        insSquare.x -= speed
-    if keys[pygame.K_w] and cy >(speed+rad):   #means clser t 0
-        cy -= speed
-        insSquare.y -= speed
-    if keys[pygame.K_s] and cy <HEIGHT -(rad):  #means clser t max value HEIGHT
-        cy += speed
-        insSquare.y += speed
+def game1():
+    while run:
+        # screen.fill(backgrnd)
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                run=False
+                print("Y quit")
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mousePos = pygame.mouse.get_pos()
+                mx = mousePos[0]
+                my = mousePos[1]
+        screen.blit(bg, (0,0))
+        keys= pygame.key.get_pressed() #this is a list
+        #mve square
+        if keys[pygame.K_RIGHT] and square.x < WIDTH -(wb):
+            square.x += speed
+            charx += speed
+        if keys[pygame.K_LEFT] and  square.x > speed:
+            square.x -= speed
+            charx -= speed
+        if keys[pygame.K_UP] and square.y >speed:   #means clser t 0
+            square.y -= speed
+            chary -= speed
+        if keys[pygame.K_DOWN] and square.y <HEIGHT -hb:  #means clser t max value HEIGHT
+            square.y += speed
+            chary += speed
+            #mve Circle
+        if keys[pygame.K_d] and cx < WIDTH -(rad):
+            cx += speed
+            insSquare.x += speed
+        if keys[pygame.K_a] and  cx > (speed+rad):
+            cx -= speed
+            insSquare.x -= speed
+        if keys[pygame.K_w] and cy >(speed+rad):   #means clser t 0
+            cy -= speed
+            insSquare.y -= speed
+        if keys[pygame.K_s] and cy <HEIGHT -(rad):  #means clser t max value HEIGHT
+            cy += speed
+            insSquare.y += speed
 
-    if square.colliderect(insSquare):
-        print("BOOM")
-        rad+=1
-        cx=random.randint(rad, WIDTH-rad)
-        cy=random.randint(rad, HEIGHT-rad)
-        ibox = rad*math.sqrt(2)
-        xig = cx-(ibox/2)
-        yig = cy-(ibox/2)
-        insSquare=pygame.Rect(xig,yig,ibox,ibox)
-    #rect(surface, color, rect) -> Rect
-    pygame.draw.rect(screen, squareClr,square)
-    screen.blit(char, (charx, chary))
-    #circle(surface, color, center, radius)
-    pygame.draw.circle(screen, circleClr, (cx,cy), rad)
-    pygame.draw.rect(screen, squareClr, insSquare)
-    pygame.display.update()
+        if square.colliderect(insSquare):
+            print("BOOM")
+            rad+=1
+            cx=random.randint(rad, WIDTH-rad)
+            cy=random.randint(rad, HEIGHT-rad)
+            ibox = rad*math.sqrt(2)
+            xig = cx-(ibox/2)
+            yig = cy-(ibox/2)
+            insSquare=pygame.Rect(xig,yig,ibox,ibox)
+        #rect(surface, color, rect) -> Rect
+        pygame.draw.rect(screen, squareClr,square)
+        screen.blit(char, (charx, chary))
+        #circle(surface, color, center, radius)
+        pygame.draw.circle(screen, circleClr, (cx,cy), rad)
+        pygame.draw.rect(screen, squareClr, insSquare)
+        pygame.display.update()
+
+def settings():
+    screen.fill(colors.get("white"))
+    Title = TITLE_FONT.render("Settings", 1, colors.get("blue"))
+    text_music = MENU_FONT.render("Music: ", 1, colors.get("blue"))
+    text1 = MENU_FONT.render("Yes", 1, colors.get("blue"))
+    text2 = MENU_FONT.render("No", 1, colors.get("blue"))
+
+    #creating button options
+    Button_1 = pygame.Rect(100, 150, 75, 75)
+    Button_2 = pygame.Rect(100, 150, 75, 75)
+    pygame.draw.rect(screen, colors.get("limeGreen"), Button_1)
+    pygame.draw.rect(screen, colors.get("limeGreen"), Button_2)  
